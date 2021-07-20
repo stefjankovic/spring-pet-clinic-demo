cf30a0beb97524087f03ad34aba6d735089a510c
sjankovic <stefan.jankovic@activitystream.com>
2021-07-20 12:55:56 +0200

    Initial commit

---
+++ src/main/java/com/demo/springpetclinicdemo/SpringPetClinicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springpetclinicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicDemoApplication.class, args);
+    }
+
+}
