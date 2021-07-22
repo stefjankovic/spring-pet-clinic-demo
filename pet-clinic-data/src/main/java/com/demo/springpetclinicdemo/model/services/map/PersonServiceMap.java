d0b152aea90c0378d4b0b26b507649fddc664810
sjankovic <stefan.jankovic@activitystream.com>
2021-07-22 11:30:33 +0200

    Impl map based services.

---
+++ pet-clinic-data/src/main/java/com/demo/springpetclinicdemo/model/services/map/PersonServiceMap.java
@@ -0,0 +1,34 @@
+package com.demo.springpetclinicdemo.model.services.map;
+
+import com.demo.springpetclinicdemo.model.model.Person;
+import com.demo.springpetclinicdemo.model.services.CrudService;
+
+import java.util.Set;
+
+public class PersonServiceMap extends AbstractMapService<Person, Long> implements CrudService<Person, Long> {
+
+    @Override
+    public Set<Person> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Person findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Person save(Person object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Person object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
