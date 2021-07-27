cc894db7e0bdd9fac568a5123c021d1d6689dc00
sjankovic <stefan.jankovic@activitystream.com>
2021-07-27 14:00:05 +0200

    added Specialty pojo and added to Vet. closes #25

--- pet-clinic-data/src/main/java/com/demo/springpetclinicdemo/model/model/Vet.java
+++ pet-clinic-data/src/main/java/com/demo/springpetclinicdemo/model/model/Vet.java
@@ -1,4 +1,16 @@
 package com.demo.springpetclinicdemo.model.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
